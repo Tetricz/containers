88a2b11d4ca1c3888faf510db8c003756d4b1f44
Tetricz <DavidLeeDaniels@protonmail.com>
2023-06-01 05:35:43 -0500

    add project zomboid

---
+++ project-zomboid/appdata/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "---Starting...---"
+term_handler() {
+	kill -SIGINT $(pidof ProjectZomboid64)
+	tail --pid=$(pidof ProjectZomboid64) -f 2>/dev/null
+	sleep 0.5
+	exit 143;
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+
+"/bin/start-server.sh" &
+killpid="$!"
+while true
+do
+	wait $killpid
+	exit 0;
+done
+
+
