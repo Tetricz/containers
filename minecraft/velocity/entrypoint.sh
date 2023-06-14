bf817b348846476badc78174b6806c4c45bf5962
Tetricz <DavidLeeDaniels@protonmail.com>
2023-06-14 14:10:26 -0500

    app -> config

--- minecraft/velocity/entrypoint.sh
+++ minecraft/velocity/entrypoint.sh
@@ -4,11 +4,11 @@ exit_result=$(echo $?)
 if [ ${exit_result} == 0 ]; then
     echo "GID exists, adding existing group..."
     GROUP=$(getent group ${GID} | cut -d: -f1)
-    adduser -g "Velocity server user" -h "/app" -s "/bin/bash" -D -u ${UID} --ingroup ${GROUP} velocity
+    adduser -g "Velocity server user" -h "/config" -s "/bin/bash" -D -u ${UID} --ingroup ${GROUP} velocity
 else
     echo "GID does not exist, creating group..."
     addgroup -g ${GID} velocity
-    adduser -g "Velocity server user" -h "/app" -s "/bin/bash" -D -u ${UID} --ingroup velocity velocity
+    adduser -g "Velocity server user" -h "/config" -s "/bin/bash" -D -u ${UID} --ingroup velocity velocity
     addgroup velocity users
 fi
 
