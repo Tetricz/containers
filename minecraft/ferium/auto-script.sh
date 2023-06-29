5e677a24822a793afe5c400f5748848adb53e1c1
Tetricz <DavidLeeDaniels@protonmail.com>
2023-06-29 03:13:37 -0500

    add permission

--- minecraft/ferium/auto-script.sh
+++ minecraft/ferium/auto-script.sh
@@ -5,6 +5,7 @@ echo -e "Container-Dir: $(pwd)/scripts/"
 
 for file in $(ls scripts | grep -E ".*\.sh");
 do
+    chmod +x /minecraft/scripts/$file
     echo -e "Loading script: $file"
     source /minecraft/scripts/$file
 done
