91a7d98ea4b86e3b452d4e56e3bac282f9ef902c
Tetricz <DavidLeeDaniels@protonmail.com>
2023-11-27 10:01:34 +0000

    CHG: do it manually

--- nextcloud/build.sh
+++ nextcloud/build.sh
@@ -9,14 +9,14 @@ docker buildx create --name nc-multi-arch-builder --bootstrap --use
 docker buildx inspect --bootstrap
 
 echo -e "Building images"
-docker buildx build --build-arg NC_VERSION=27 --platform=linux/amd64 -t tetricz/nextcloud:amd64-27 . --load
-docker buildx build --build-arg NC_VERSION=26 --platform=linux/amd64 -t tetricz/nextcloud:amd64-26 . --load
-docker buildx build --build-arg NC_VERSION=25 --platform=linux/amd64 -t tetricz/nextcloud:amd64-25 . --load
+docker buildx build --build-arg NC_VERSION="27.1.3" --platform=linux/amd64 -t tetricz/nextcloud:amd64-27 . --load
+# docker buildx build --build-arg NC_VERSION=26 --platform=linux/amd64 -t tetricz/nextcloud:amd64-26 . --load
+# docker buildx build --build-arg NC_VERSION=25 --platform=linux/amd64 -t tetricz/nextcloud:amd64-25 . --load
 
 echo -e "Pushing images"
 docker push tetricz/nextcloud:amd64-27
-docker push tetricz/nextcloud:amd64-26
-docker push tetricz/nextcloud:amd64-25
+# docker push tetricz/nextcloud:amd64-26
+# docker push tetricz/nextcloud:amd64-25
 
 echo -e "Creating manifest"
 docker manifest create tetricz/nextcloud:latest tetricz/nextcloud:amd64-27 --amend
